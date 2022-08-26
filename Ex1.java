3b61d7a3155b00f2eea1c7278e7f7061a7dd9dd1
Melissa <91746003+melpompom@users.noreply.github.com>
2022-08-26 11:07:12 -0300

    Add files via upload

---
+++ Ex1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		float nota;
+
+		do {
+			System.out.println("Informe uma nota(de 0 - 10): ");
+			nota = input.nextFloat();
+			if (nota > 10 || nota < 0) {
+				System.out.println("Valor Inválido!");
+			}
+		} while (nota > 10 || nota < 0);
+		System.out.println("Valor Válido!");
+		input.close();
+	}
+
+}
